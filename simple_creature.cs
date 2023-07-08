c55e6382f18788dfa2b912496e8690e11c894ee4
MuradKarimov <muradhafizoglu.kr@gmail.com>
2023-07-08 10:11:08 +0400

    simple creature

---
+++ simple_creature.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Backend02
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("  .-. ");
+            Console.WriteLine(" (o o) ");
+            Console.WriteLine(" | O | ");
+            Console.WriteLine(" |   | ");
+            Console.WriteLine(" '~~~' ");
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+    }
+}
